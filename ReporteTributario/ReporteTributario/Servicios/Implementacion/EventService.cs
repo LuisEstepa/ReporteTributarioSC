bdfd507e5808255c486f49b195841ae92f6e276b
BelfastA <angelblack_41@hotmail.com>
2024-01-14 22:33:19 -0500

    Implementacion de calendario

---
+++ ReporteTributario/ReporteTributario/Servicios/Implementacion/EventService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReporteTributario.Models;
+using ReporteTributario.Models.Entities;
+using ReporteTributario.Servicios.Contrato;
+
+namespace ReporteTributario.Servicios.Implementacion
+{
+    public class EventService : IEventService
+    {
+        private readonly DbTtributarioContext _Context;
+        public EventService(DbTtributarioContext dbContext)
+        {
+            _Context = dbContext;
+        }
+        public async Task<List<InformacionBase>> GetInformacionListAsync()
+        {
+            List<InformacionBase> _event = await _Context.InformacionBase.Where(x => x.Vigente == true).ToListAsync();
+            return _event;
+        }
+    }
+}
---
+++ ReporteTributario/ReporteTributario/wwwroot/js/Calendario/main.js
@@ -0,0 +1,97 @@
+﻿import { Calendar } from "@fullcalendar/core";
+import LocaleEs from "@fullcalendar/core/locales/es";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import EventManager from "./scripts/EventManager";
+import listPlugin from "@fullcalendar/list";
+import "@dile/dile-modal/dile-modal";
+import "./css/Calendario/StyleCalendario.css";
+
+
+const container = document.querySelector(".calendar-container");
+const eventModal = document.querySelector("#event-modal");
+const form = eventModal.querySelector("form");
+let selectedInfo = null;
+let selectedEvent = null;
+
+const eventManager = new EventManager();
+
+const handleOnSelect = (info) => {
+    console.log("selected " + info.startStr + " to " + info.endStr);
+    selectedInfo = info;
+    eventModal.open();
+};
+
+
+const handleOnClickEvent = (data) => {
+    form.querySelector('[name="title"]').value = data.event.title;
+    form.querySelector('[name="description"]').value =
+        data.event.extendedProps.description;
+    selectedEvent = data.event;
+    eventModal.querySelector(".delete-event-btn").classList.remove("d-none");
+    eventModal.querySelector("button[type='submit']").innerHTML = "Editar";
+    eventModal.open();
+};
+
+
+const handleOnSubmitForm = (e) => {
+    e.preventDefault();
+    const title = e.target.querySelector('[name="title"]').value;
+    const description = e.target.querySelector('[name="description"]').value;
+    if (!title.trim() || !description.trim()) {
+        return;
+    }
+    if (selectedEvent) {
+        selectedEvent.setProp("title", title);
+        selectedEvent.setExtendedProp("description", description);
+        eventManager.updateEvent({ title, description }, selectedEvent.id);
+    } else {
+        const event = {
+            id: `${Date.now()}`,
+            title,
+            extendedProps: {
+                description,
+            },
+            start: selectedInfo.startStr,
+            end: selectedInfo.endStr,
+        };
+        eventManager.saveEvent(event);
+        calendar.addEvent(event);
+    }
+    eventModal.close();
+};
+
+
+eventModal.querySelector(".delete-event-btn").addEventListener("click", () => {
+    eventManager.deleteEvent(selectedEvent.id);
+    selectedEvent.remove();
+    eventModal.close();
+});
+form.addEventListener("submit", handleOnSubmitForm);
+eventModal.addEventListener("dile-modal-closed", () => {
+    form.querySelector('[name="title"]').value = "";
+    form.querySelector('[name="description"]').value = "";
+    eventModal.querySelector(".delete-event-btn").classList.add("d-none");
+    eventModal.querySelector("button[type='submit']").innerHTML = "Guardar";
+    selectedInfo = null;
+    selectedEvent = null;
+});
+
+
+const calendar = new Calendar(container, {
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+    initialView: "dayGridMonth",
+    longPressDelay: 100,
+    locale: LocaleEs,
+    headerToolbar: {
+        left: "prev,next today",
+        center: "title",
+        right: "dayGridMonth,timeGridWeek,listWeek",
+    },
+    selectable: true,
+    events: eventManager.getEvents(),
+    eventClick: handleOnClickEvent,
+    select: handleOnSelect,
+});
+calendar.render();
