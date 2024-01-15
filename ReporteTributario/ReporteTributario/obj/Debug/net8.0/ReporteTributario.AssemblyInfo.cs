6ce1dade26f1b6db606a69ac6e1d5724e967c04e
Luis Estepa <116852447+LuisEstepa@users.noreply.github.com>
2024-01-15 08:31:30 -0500

    confirma

--- ReporteTributario/ReporteTributario/obj/Debug/net8.0/ReporteTributario.AssemblyInfo.cs
+++ ReporteTributario/ReporteTributario/obj/Debug/net8.0/ReporteTributario.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReporteTributario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7949b073bb7794cdece0af87c511b6e3ef5fb183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReporteTributario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReporteTributario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
